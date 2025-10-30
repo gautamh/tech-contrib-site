63fd866fcc4dd7adcbdbee4ffa67c30b65786741
Gautam Hathi <gautamhathi@live.com>
2025-10-29 23:24:46 -0400

    adding +layout.svelte with Google Analytics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<svelte:head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163480621-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-163480621-1');
+        gtag('config', 'G-YEVD177Z78');
+    </script>
+    
+    <meta charset="utf-8">
+    <meta name="description" content="Tech campaign contributions">
+    <meta name="author" content="Gautam Hathi">
+</svelte:head>
+<body>
+    <main>
+        <slot />
+    </main>
+</body>
